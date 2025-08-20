5414e03f4e8a28a64a2f866d504a9e37032604e9
qiqioberon <aquq1q1.farrukh@gmail.com>
2025-08-20 21:56:39 +0700

    feat: add wildcard ngrok

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "*.ngrok-free.app", // Wildcard untuk semua subdomain ngrok
+        port: "",
+        pathname: "/uploads/**",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
         port: "3001", // Sesuaikan port jika backend Anda berjalan di port lain
@@ -10,7 +16,7 @@ const nextConfig = {
       },
       {
         protocol: "https", // Ganti ke http jika backend production Anda tidak pakai https
-        hostname: "https://mako-related-miserably.ngrok-free.app",
+        hostname: "mako-related-miserably.ngrok-free.app",
         port: "", // Kosongkan jika pakai port default (443 untuk https)
         pathname: "/uploads/**", // Pastikan path ini sesuai
       },
